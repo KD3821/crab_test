167cc928fce1b842c117cdb5c12813da32ab304f
KD3821 <80853241+KD3821@users.noreply.github.com>
2023-08-13 19:30:28 +0300

    feat: add docker files and confs for db django nginx

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM python:3.9-slim
+
+SHELL ["/bin/bash", "-c"]
+
+ENV PYTHONDONTWRITEBYTECODE=1 PYTHONUNBUFFERED=1
+
+RUN pip install --upgrade pip
+
+RUN apt update && apt -qy install gcc libjpeg-dev libpq-dev postgresql-client
+
+RUN useradd -rms /bin/sash quiz_user && chmod 777 /opt /run
+
+WORKDIR /quiz_app
+
+RUN mkdir /quiz_app/static && mkdir /quiz_app/media && chown -R quiz_user:quiz_user /quiz_app && chmod 755 /quiz_app
+
+COPY --chown=quiz_user:quiz_user . .
+
+RUN pip install -r requirements.txt
+
+USER quiz_user
+
+CMD ["gunicorn","-b","0.0.0.0:8001","tests_app.wsgi:application"]
--- tests_app/settings.py
+++ tests_app/settings.py
@@ -127,11 +127,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = 'static'
 
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = 'media/'
+MEDIA_ROOT = 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -140,7 +139,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'accounts.User'
 
-try:
-    from .settings_local import *
-except ImportError:
-    pass
+# try:
+#     from .settings_local import *
+# except ImportError:
+#     pass
