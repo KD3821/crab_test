96f4727817b61e14de6c10a29981ccc2ac838c1a
KD3821 <80853241+KD3821@users.noreply.github.com>
2023-08-18 00:26:59 +0300

    test: remove nginx

--- tests_app/settings.py
+++ tests_app/settings.py
@@ -127,10 +127,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = 'static'
+STATIC_ROOT = '/var/www/crab_quiz/static/'
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = 'media'
+MEDIA_ROOT = '/var/www/crab_quiz/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
