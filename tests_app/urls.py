0fe9c04525665e7c1600da365aa59cdde676b4db
KD3821 <80853241+KD3821@users.noreply.github.com>
2023-08-17 20:13:11 +0300

    test: add host for nginx

--- tests_app/urls.py
+++ tests_app/urls.py
@@ -17,10 +17,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from accounts.views import login_view
+
 
 urlpatterns = [
+    path('', login_view),
     path('admin/', admin.site.urls),
     path('quiz/', include(('quiz.urls', 'quiz'))),
     path('accounts/', include(('accounts.urls', 'accounts'))),
 ]
+
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
